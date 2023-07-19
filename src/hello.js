8033e385e05c3d97e08ea62692c391a2ee0bbcc1
Thasi Pablo <39631185+thasipablo@users.noreply.github.com>
2023-07-19 02:25:23 +0100

    webpack setup

---
+++ src/hello.js
@@ -0,0 +1,3 @@
+const sayHello = () => "Hello world!";
+
+export default sayHello;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import {v4 as uuidv4} from 'uuid';
+import sayHello from "./hello.js";
+import './styles/style.scss'
+
+console.log(sayHello());
+console.log(uuidv4());
+console.log("You're welcome to join");
